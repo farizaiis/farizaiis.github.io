f057926979a136af7e1a26347b7cc1d9461eb723
farizaiis <farizaiis@hotmail.com>
2021-11-24 02:09:09 +0700

    modify dark-mode at css

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ function myFunction() {
     var element = document.getElementById('experiencepage');
     element.classList.toggle('dark-mode');
 
+    var element = document.getElementById('contactpage');
+    element.classList.toggle('dark-mode');
+
     var element = document.getElementById('kontenpage');
     element.classList.toggle('dark-mode');
 
-    var element = document.getElementById('contactpage');
+    var element = document.getElementById('clientpage');
     element.classList.toggle('dark-mode');
 }
 
@@ -17,6 +20,4 @@ function onSubmit(e) {
         company: document.querySelector('input[name = company]').value,
         message: document.querySelector('textarea[name = message]').value,
     };
-    
-    
 }
