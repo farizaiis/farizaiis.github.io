941fa20ed0dba6564128e69b3ea21e5e313c2a25
farizaiis <farizaiis@hotmail.com>
2021-11-23 23:25:56 +0700

    revisi modal

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function myFunction() {
-    var element = document.getElementById("experiencepage");
-    element.classList.toggle("dark-mode");
+    var element = document.getElementById('experiencepage');
+    element.classList.toggle('dark-mode');
 
-    var element = document.getElementById("kontenpage");
-    element.classList.toggle("dark-mode");
+    var element = document.getElementById('kontenpage');
+    element.classList.toggle('dark-mode');
 
-    var element = document.getElementById("contactpage");
-    element.classList.toggle("dark-mode");
-};
+    var element = document.getElementById('contactpage');
+    element.classList.toggle('dark-mode');
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    let data = {
+        email: document.querySelector('input[name = email]').value,
+        company: document.querySelector('input[name = company]').value,
+        message: document.querySelector('textarea[name = message]').value,
+    };
+    
+    
+}
