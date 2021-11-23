654dc7dc6f1e9b8f50e9164b141bd60d4880a2cf
farizaiis <farizaiis@hotmail.com>
2021-11-23 21:09:34 +0700

    add modal and try download

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.getElementById("experiencepage");
+    element.classList.toggle("dark-mode");
+
+    var element = document.getElementById("kontenpage");
+    element.classList.toggle("dark-mode");
+
+    var element = document.getElementById("contactpage");
+    element.classList.toggle("dark-mode");
+};
