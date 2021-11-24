c3ffb0cda49e6c5c2f39ac93ff78b21e99b9882a
farizaiis <farizaiis@hotmail.com>
2021-11-24 14:24:38 +0700

    deploy

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const Router = require('./routes/index');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/v1', Router);
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'server running',
-        serverTime: new Date()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-});
+}
 
 app.get('*', function (req, res) {
     res.status(404).send('not found');
