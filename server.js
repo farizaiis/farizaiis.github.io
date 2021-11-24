94ac2eb10a15f0cc3d7429f2d65926a11d516c5e
farizaiis <farizaiis@hotmail.com>
2021-11-24 14:27:15 +0700

    deploy

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use('/v1', Router);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('client'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
     });
 }
 
