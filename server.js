8fd0e4d8bca95acfbab50b73d63712ac49f1a81a
farizaiis <farizaiis@hotmail.com>
2021-11-24 14:25:56 +0700

    deploy

--- server.js
+++ server.js
@@ -11,13 +11,9 @@ app.use('/v1', Router);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-app.get('*', function (req, res) {
-    res.status(404).send('not found');
-});
 
 module.exports = app;
